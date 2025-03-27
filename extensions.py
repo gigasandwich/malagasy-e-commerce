9fd3914b3f80f3e9516baac725486fc597bf81c5
Yvan Noah <yvannandy@gmail.com>
2025-03-27 23:54:58 +0300

    Add module run-train-test to easily train and test the model

--- extensions.py
+++ extensions.py
@@ -1,3 +1,7 @@
 # Module created to get rid of circular import
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+# Folders for training
+raw_data_folder = 'data/raw-data'
+trained_models_folder = 'data/trained-models'
